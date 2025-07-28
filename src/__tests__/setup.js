963c678789460bc6185bee059233ad88a5de2135
ryangachahic <ryangachahi230@gmail.com>
2025-07-28 22:09:50 +0300

    intial commit

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,9 +1,52 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
-import 'whatwg-fetch';
-import {Blob} from 'node:buffer';
+import { vi } from 'vitest';
 
-afterEach(() => {
-    cleanup();
-})
+global.fetch = vi.fn();
+
+beforeEach(() => {
+  fetch.mockReset();
+  fetch.mockImplementation((url) => {
+    if (url === 'http://localhost:3000/movies') {
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            { id: 1, title: 'Doctor Strange', time: 115, genres: ['Action', 'Adventure', 'Fantasy'] },
+            { id: 2, title: 'Trolls', time: 92, genres: ['Animation', 'Comedy'] },
+            { id: 3, title: 'Moonlight', time: 111, genres: ['Drama'] },
+          ]),
+      });
+    }
+    if (url === 'http://localhost:3000/movies/1') {
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            id: 1,
+            title: 'Doctor Strange',
+            time: 115,
+            genres: ['Action', 'Adventure', 'Fantasy'],
+          }),
+      });
+    }
+    if (url === 'http://localhost:3000/actors') {
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            { id: 1, name: 'Benedict Cumberbatch', movies: ['Doctor Strange', 'The Imitation Game', 'Black Mass'] },
+            { id: 2, name: 'Justin Timberlake', movies: ['Trolls', 'Friends with Benefits', 'The Social Network'] },
+            { id: 3, name: 'Anna Kendrick', movies: ['Pitch Perfect', 'Into The Wood'] },
+            { id: 4, name: 'Tom Cruise', movies: ['Jack Reacher: Never Go Back', 'Mission Impossible 4', 'War of the Worlds'] },
+          ]),
+      });
+    }
+    if (url === 'http://localhost:3000/directors') {
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            { id: 1, name: 'Scott Derrickson', movies: ['Doctor Strange', 'Sinister', 'The Exorcism of Emily Rose'] },
+            { id: 2, name: 'Mike Mitchell', movies: ['Trolls', 'Alvin and the Chipmunks: Chipwrecked', 'Sky High'] },
+            { id: 3, name: 'Edward Zwick', movies: ['Jack Reacher: Never Go Back', 'Blood Diamond', 'The Siege'] },
+          ]),
+      });
+    }
+    return Promise.reject(new Error('Unknown URL'));
+  });
+});
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,24 @@
-import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+
+
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
-    );
-};
+    <div className="navbar">
+      <nav role="navigation">
+        <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+          Home
+        </NavLink>
+        <NavLink to="/directors" className={({ isActive }) => (isActive ? 'active' : '')}>
+          Directors
+        </NavLink>
+        <NavLink to="/actors" className={({ isActive }) => (isActive ? 'active' : '')}>
+          Actors
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
 
 export default NavBar;
